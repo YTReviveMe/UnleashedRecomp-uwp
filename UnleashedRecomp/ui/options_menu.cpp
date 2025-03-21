c0a2d9e4cdb2ba0c496d125e626ef7aad921aabd
= <danworley@gmail.com>
2025-03-21 11:55:37 -0400

    Disable game breaking options on UWP

--- UnleashedRecomp/ui/options_menu.cpp
+++ UnleashedRecomp/ui/options_menu.cpp
@@ -1269,6 +1269,7 @@ static void DrawConfigOptions()
             DrawConfigOption(rowCount++, yOffset, &Config::ResolutionScale, true, nullptr, 0.25f, 1.0f, 2.0f);
             DrawConfigOption(rowCount++, yOffset, &Config::Fullscreen, true);
             DrawConfigOption(rowCount++, yOffset, &Config::VSync, true);
+            DrawConfigOption(rowCount++, yOffset, &Config::TripleBuffering, true); // TODO: Needs translation
             DrawConfigOption(rowCount++, yOffset, &Config::FPS, true, nullptr, FPS_MIN, 120, FPS_MAX);
             DrawConfigOption(rowCount++, yOffset, &Config::Brightness, true);
             DrawConfigOption(rowCount++, yOffset, &Config::AntiAliasing, true);
